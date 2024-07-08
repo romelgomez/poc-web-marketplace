13c33abe31eb58f048827731fa5d820b48744e09
romelgomez <bmxandcode@gmail.com>
2024-07-07 22:49:10 -0500

    chore: log abvances

---
+++ builld_develop.sh
@@ -0,0 +1,3 @@
+# docker-compose -f docker-compose-develop.yml down -v
+# docker build --progress=plain --no-cache -t api-develop -f apps/api/Dockerfile.develop .
+# docker-compose -f docker-compose-develop.yml up
