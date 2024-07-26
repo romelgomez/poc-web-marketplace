04eb81a0d4ecd4cb987709d4aab0eb906fef1201
romelgomez <bmxandcode@gmail.com>
2024-07-26 14:46:21 -0500

    chore: removing a unsed file

--- builld_develop.sh
+++
@@ -1,3 +0,0 @@
-# docker-compose -f docker-compose-develop.yml down -v
-# docker build --progress=plain --no-cache -t api-develop -f apps/api/Dockerfile.develop .
-# docker-compose -f docker-compose-develop.yml up
