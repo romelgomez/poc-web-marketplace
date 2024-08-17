93436632ec1394c1346a2e6b278b7a66be5ab30b
romelgomez <bmxandcode@gmail.com>
2024-08-17 12:17:08 -0500

    fix: load end from the build

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -17,7 +17,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ClerkProvider
       {...pageProps}
-      publishableKey={`${process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}`}
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
     >
       <AntdConfigProvider locale={esES}>
         <ApolloProviderWrapper>
